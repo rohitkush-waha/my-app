5a0396bc5c21d4542f028984d9c0eecbe7c8f62c
Rohit Kushwaha <rohitkushwaha9801011@gmail.com>
2023-04-17 10:07:04 +0530

    fdffhyhh

---
+++ src/screen/SadToQar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SadToQar() {
+  return (
+    <div>
+        <div>
+            <h1>Travelling Saudi Arabia From Qatar</h1>
+            
+        </div>
+    </div>
+  )
+}
+
+export default SadToQar
